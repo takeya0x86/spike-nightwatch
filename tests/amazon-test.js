d55e3f0505ab28de109a5af3f0f20305cef4b02b
Takeshi Kishi <takeya0x86@gmail.com>
2018-10-06 23:06:49 +0900

    テストを修正

--- tests/amazon-test.js
+++ tests/amazon-test.js
@@ -28,11 +28,11 @@ module.exports = {
     browser
         .url(browser.launch_url)
         .waitForElementVisible('body', 1000)
-        .setValue('input[type=text]', 'nightwatch')
-        .waitForElementVisible('button[name=btnG]', 1000)
-        .click('button[name=btnG]')
+        .setValue('#twotabsearchtextbox', 'nightwatch')
+        .waitForElementVisible('#nav-search-submit-text', 1000)
+        .click('#nav-search-submit-text')
         .pause(1000)
-        .assert.containsText('#main', 'Night Watch')
+        .assert.containsText('body', 'プライム')
         .end();
   },
 
